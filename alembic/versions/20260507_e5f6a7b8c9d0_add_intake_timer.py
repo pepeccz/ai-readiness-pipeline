99a0d8c898517d65c54b8a2ea6d30972ef898c50
Pepe <pepecz6305@gmail.com>
2026-05-07 15:20:53 +0200

    fix(alembic): unique revision id for add_intake_timer migration
    Previous revision id (c3d4e5f6a7b8) collided with an existing migration
    and pointed to the wrong parent (b2c3d4e5f6a7 instead of the actual head
    d4e5f6a7b8c9), causing alembic to refuse upgrade with "Multiple head
    revisions are present".
    
    New id e5f6a7b8c9d0 chained correctly onto d4e5f6a7b8c9.

--- alembic/versions/20260507_e5f6a7b8c9d0_add_intake_timer.py
+++ alembic/versions/20260507_e5f6a7b8c9d0_add_intake_timer.py
@@ -1,7 +1,7 @@
 """add_intake_timer
 
-Revision ID: c3d4e5f6a7b8
-Revises: b2c3d4e5f6a7
+Revision ID: e5f6a7b8c9d0
+Revises: d4e5f6a7b8c9
 Create Date: 2026-05-07 00:00:00.000000+00:00
 
 REQ-1: Add three timer columns to intake_sessions:
@@ -19,8 +19,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c3d4e5f6a7b8'
-down_revision: Union[str, None] = 'b2c3d4e5f6a7'
+revision: str = 'e5f6a7b8c9d0'
+down_revision: Union[str, None] = 'd4e5f6a7b8c9'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
